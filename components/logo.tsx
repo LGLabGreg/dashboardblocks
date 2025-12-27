4a450448b9b84a585d550af09988bb7b3ab8be75
LGLabGreg <greg@lglab.co.uk>
2025-12-27 08:20:42 +0000

    fix: logo link + deps

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,14 +1,12 @@
-import Link from 'next/link'
-
 import { cn } from '@/lib/utils'
 
 import { Icons } from './icons'
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Link href='/' className={cn('flex items-center gap-1 text-xl font-bold', className)}>
+    <div className={cn('flex items-center gap-1 text-lg font-bold', className)}>
       <Icons.logo className='size-8 dark:invert' />
       Dashboardblocks
-    </Link>
+    </div>
   )
 }
