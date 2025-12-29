bbcf588ae3023e91b7ba06d495c8f64466e262ae
LGLabGreg <greg@lglab.co.uk>
2025-12-29 11:30:25 +0000

    nit(preview): padding

--- components/component-preview-highlighted.tsx
+++ components/component-preview-highlighted.tsx
@@ -36,7 +36,7 @@ export function ComponentPreviewHighlighted({
   }, [code])
 
   return (
-    <div className={cn('not-prose my-6 border border-dashed p-6', className)}>
+    <div className={cn('not-prose my-6 border border-dashed p-4', className)}>
       <Tabs defaultValue='preview'>
         <div className='flex flex-wrap items-center gap-2'>
           <TabsList>
